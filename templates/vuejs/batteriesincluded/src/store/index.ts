3c9298fe00afa9b8d94fd3020a9bc95c57481ed4
Radmacher <66096031+rdmchr@users.noreply.github.com>
2021-10-20 00:57:24 +0200

    Merge pull request #4 from rdmchr/vue-batteries-included
    Set up the Vue.JS batteries-included template

---
+++ templates/vuejs/batteriesincluded/src/store/index.ts
@@ -0,0 +1,11 @@
+import {defineStore} from 'pinia';
+
+
+export const useStore = defineStore('main', {
+    state() {
+        return {
+            counter: 1 as number,
+        };
+    },
+    actions: {},
+});
