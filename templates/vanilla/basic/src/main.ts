6d85280ed6e342dae5b5a2146ca8bcfcccfde9a9
Radmacher <66096031+rdmchr@users.noreply.github.com>
2021-10-20 18:51:28 +0200

    Added Vanilla JS basic template (#10)

---
+++ templates/vanilla/basic/src/main.ts
@@ -0,0 +1,10 @@
+import './style.css'
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+app.innerHTML = `
+  <h1>Hello {{projectName}}!</h1>
+  <p>This is a Vite project created by the Appwrite Starter.</p>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a>
+  <a href="https://github.com/rdmchr/Appwrite-Starter" target="_blank">Appwrite Starter</a>
+`
