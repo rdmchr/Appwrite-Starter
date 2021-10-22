79d9318153ab8bad389fdf57e6afa46f60f3d7bb
Radmacher <66096031+rdmchr@users.noreply.github.com>
2021-10-22 22:19:18 +0200

    Added Prettier (#24)
    * Installed Prettier
    
    * Added Prettier configuration
    
    * Added Prettier scripts
    
    * Added Prettier GitHub workflow
    
    * Used Prettier to fix styling
    
    * Fixed Prettier job name
    
    * Build workflow now depends on the Prettier workflow

--- src/templateEngine.ts
+++ src/templateEngine.ts
@@ -10,26 +10,26 @@ import {moustacheData} from './interfaces';
  * @param moustaches the moustaches and their data that should be replaced
  */
 function format(text: string | Buffer, moustaches: moustacheData) {
-    const template = compile(text.toString(), {noEscape: true});
-    return template(moustaches);
+  const template = compile(text.toString(), {noEscape: true});
+  return template(moustaches);
 }
 
 /*
     Returns a list of all files in a directory
  */
 async function goThroughDir(dir: string) {
-    let fileList: any[] = [];
+  let fileList: any[] = [];
 
-    const files = await fs.readdir(dir);
-    for (const file of files) {
-        const path = join(dir, file);
-        if ((await fs.stat(path)).isDirectory()) {
-            fileList = [...fileList, ...(await goThroughDir(path))];
-        } else {
-            fileList.push(path);
-        }
+  const files = await fs.readdir(dir);
+  for (const file of files) {
+    const path = join(dir, file);
+    if ((await fs.stat(path)).isDirectory()) {
+      fileList = [...fileList, ...(await goThroughDir(path))];
+    } else {
+      fileList.push(path);
     }
-    return fileList;
+  }
+  return fileList;
 }
 
 /**
@@ -37,13 +37,12 @@ async function goThroughDir(dir: string) {
  * @param filePath the path of the project directory
  */
 async function prepareDir(filePath: string) {
-    const target = dirname(filePath);
-    try {
-        await fs.mkdir(target, {recursive: true});
-    } catch {
-        throw new Error(`Cannot create directory with the path ${target}`);
-    }
-
+  const target = dirname(filePath);
+  try {
+    await fs.mkdir(target, {recursive: true});
+  } catch {
+    throw new Error(`Cannot create directory with the path ${target}`);
+  }
 }
 
 /**
@@ -52,22 +51,24 @@ async function prepareDir(filePath: string) {
  * @param templateDir the path to the template directory
  * @param moustaches the moustache data
  */
-async function copy(projectDir: string, templateDir: string, moustaches: moustacheData) {
-    const templateFiles = await goThroughDir(templateDir);
-    for (const sourcePath of templateFiles) {
-        const relativePath = relative(templateDir, sourcePath);
-        const targetPath = format(resolve(projectDir, relativePath), moustaches);
-        await prepareDir(targetPath);
+async function copy(
+  projectDir: string,
+  templateDir: string,
+  moustaches: moustacheData
+) {
+  const templateFiles = await goThroughDir(templateDir);
+  for (const sourcePath of templateFiles) {
+    const relativePath = relative(templateDir, sourcePath);
+    const targetPath = format(resolve(projectDir, relativePath), moustaches);
+    await prepareDir(targetPath);
 
-        let source = await fs.readFile(sourcePath);
-        let target = source;
-        if (isUtf8(source)) {
-            target = Buffer.from(format(source, moustaches));
-        }
-        await fs.writeFile(targetPath, target, 'utf-8');
+    let source = await fs.readFile(sourcePath);
+    let target = source;
+    if (isUtf8(source)) {
+      target = Buffer.from(format(source, moustaches));
     }
+    await fs.writeFile(targetPath, target, 'utf-8');
+  }
 }
 
-export {
-    copy,
-};
+export {copy};
